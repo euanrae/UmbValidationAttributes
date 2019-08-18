6116c3e808a91da471c0e14d16eff4b7ea2fe330
Euan Rae <euan37@hotmail.com>
2019-08-18 11:27:59 +0100

    Adding docs for regex validator

--- src/UmbValidationAttributes/Attributes/UmbracoRegexAttribute.cs
+++ src/UmbValidationAttributes/Attributes/UmbracoRegexAttribute.cs
@@ -4,12 +4,25 @@ using UmbValidationAttributes.Helpers;
 
 namespace UmbValidationAttributes.Attributes
 {
+    /// <summary>
+    /// RequiredAttribute that gets error message from Umbraco
+    /// </summary>
     public class UmbracoRegexAttribute : RegularExpressionAttribute
     {
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
         public UmbracoRegexAttribute(string pattern, string messageKey) : this(pattern, messageKey, "", "")
         {
 
         }
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
+        /// <param name="fallbackMessageKey">Key for a default message if 'messageKey' value returns null</param>
+        /// <param name="defaultMessage">Hard coded default message if none is found from messageKey + fallbackMessageKey</param>
         public UmbracoRegexAttribute(string pattern, string messageKey, string fallbackMessageKey = "", string defaultMessage = "")
             :base (pattern)
         {
