086780c55adf9b3d9b3141777b0137e23376b955
Euan Rae <euan37@hotmail.com>
2019-08-18 09:18:49 +0100

    adding xml comments for package

--- src/UmbValidationAttributes/Attributes/UmbracoEmailAddressAttribute.cs
+++ src/UmbValidationAttributes/Attributes/UmbracoEmailAddressAttribute.cs
@@ -4,12 +4,26 @@ using UmbValidationAttributes.Helpers;
 
 namespace UmbValidationAttributes.Attributes
 {
+    /// <summary>
+    /// EmailAttribute that gets error message from Umbraco
+    /// </summary>
     public class UmbracoEmailAddressAttribute : BaseEmailAddressAttribute
     {
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
         public UmbracoEmailAddressAttribute(string messageKey) : this(messageKey, "", "")
         {
 
         }
+
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
+        /// <param name="fallbackMessageKey">Key for a default message if 'messageKey' value returns null</param>
+        /// <param name="defaultMessage">Hard coded default message if none is found from messageKey + fallbackMessageKey</param>
         public UmbracoEmailAddressAttribute(string messageKey, string fallbackMessageKey = "", string defaultMessage = "")
         {
             if (string.IsNullOrWhiteSpace(messageKey))
--- src/UmbValidationAttributes/Attributes/UmbracoRequiredAttribute.cs
+++ src/UmbValidationAttributes/Attributes/UmbracoRequiredAttribute.cs
@@ -4,12 +4,26 @@ using UmbValidationAttributes.Helpers;
 
 namespace UmbValidationAttributes.Attributes
 {
+    /// <summary>
+    /// RequiredAttribute that gets error message from Umbraco
+    /// </summary>
     public class UmbracoRequiredAttribute : RequiredAttribute
     {
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
         public UmbracoRequiredAttribute(string messageKey) : this(messageKey, "", "")
         {
 
         }
+
+        /// <summary>
+        /// Create with a message key - will fallback to RequiredAttribute default message if none found
+        /// </summary>
+        /// <param name="messageKey">Key for the message when validation fails</param>
+        /// <param name="fallbackMessageKey">Key for a default message if 'messageKey' value returns null</param>
+        /// <param name="defaultMessage">Hard coded default message if none is found from messageKey + fallbackMessageKey</param>
         public UmbracoRequiredAttribute(string messageKey, string fallbackMessageKey = "", string defaultMessage = "")
         {
             if (string.IsNullOrWhiteSpace(messageKey))
